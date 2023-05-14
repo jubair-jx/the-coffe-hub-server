d513b17ebc5c1fe0a6c60a83a97bd074628b94e8
jubair-jx <jubair.official97@gmail.com>
2023-05-14 17:36:05 +0600

    Server Update Functionlity done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Boss You Coffe Server is Running Here");
+  res.send("Boss You coffes Server is Running Here");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.udnr6tc.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,23 +29,54 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const coffeCollection = client.db("coffedb").collection("coffe");
+    const coffesCollection = client.db("coffedb").collection("coffe");
 
-    app.get("/coffe", async (req, res) => {
-      const cursor = coffeCollection.find();
+    app.get("/coffes", async (req, res) => {
+      const cursor = coffesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.delete("/coffe/:id", async (req, res) => {
+    app.get("/coffes/:id", async (req, res) => {
       const id = req.params.id;
+      console.log(id);
       const query = { _id: new ObjectId(id) };
-      const result = await coffeCollection.deleteOne(query);
+
+      const result = await coffesCollection.findOne(query);
+
       res.send(result);
     });
-    app.post("/coffe", async (req, res) => {
+    app.put("/coffes/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateCoffe = req.body;
+      console.log(id, filter, options, updateCoffe);
+      const coffes = {
+        $set: {
+          name: updateCoffe.name,
+          supplierName: updateCoffe.supplierName,
+          category: updateCoffe.category,
+          chef: updateCoffe.chef,
+          taste: updateCoffe.taste,
+          details: updateCoffe.details,
+          photoUrl: updateCoffe.photoUrl,
+        },
+      };
+
+      const result = await coffesCollection.updateOne(filter, coffes, options);
+      console.log(result);
+      res.send(result);
+    });
+    app.delete("/coffes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffesCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.post("/coffes", async (req, res) => {
       const data = req.body;
       console.log(data);
-      const collection = await coffeCollection.insertOne(data);
+      const collection = await coffesCollection.insertOne(data);
       res.send(collection);
     });
     await client.db("admin").command({ ping: 1 });
@@ -58,5 +89,5 @@ run().catch(console.dir);
 
 //server port
 app.listen(PORT, () => {
-  console.log(`Here is Coffe Hub Server Port : ${PORT}`);
+  console.log(`Here is coffes Hub Server Port : ${PORT}`);
 });
